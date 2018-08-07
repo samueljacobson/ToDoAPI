c3cdf7105be17879d4e4905761b4a1fa72a42539
samueljacobson <samuel.c.jacobson@gmail.com>
2018-08-07 11:40:20 -0400

    first commit

---
+++ ToDoAPI.Tests/ToDoControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAPI.Controllers;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests
+{
+    [TestFixture]
+    public class ToDoControllerTests
+    {
+        //[Test]
+        //public void Test()
+        //{
+        //    Assert.Fail();
+        //}
+
+        [Test]
+        public void GetAll_Returns_A_List_Of_ToDos()
+        {
+            var controller = new ToDoController();
+            var result = controller.GetAll();
+            Assert.That(result.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Post_Saves_ToDo()
+        {
+            var todo = new ToDo();
+            var controller = new ToDoController();
+
+            controller.Post(todo);
+
+            var result = controller.GetAll();
+
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ ToDoAPI/Controllers/ToDoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    public class ToDoController : ApiController
+    {
+        private static List<ToDo> todos = new List<ToDo>()
+            {
+                new ToDo() { Id = 1, Name = "Feed cat", Priority = 1}
+            };
+
+    public List<ToDo> GetAll()
+        {
+            return todos;
+        }
+
+        public void Post(ToDo todo)
+        {
+            todos.Add(todo);
+        }
+    }
+}
